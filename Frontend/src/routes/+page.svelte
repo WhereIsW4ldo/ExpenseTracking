a838271b5a47ff157326656fe0d42d56e15f814b
waldo.wautelet <waldo.wautelet@twipemobile.com>
2024-03-17 14:59:01 +0100

    initial commit FE development

---
+++ Frontend/src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	export let categories = {
+		'1': 'Food',
+		'2': 'House'
+	};
+
+	async function GetCategories() {
+        fetch ("http://localhost:5298/CategoryController/")
+        .then(response => response.json)
+        .then(data => {
+            console.log(data);
+            categories = data;
+        })
+    }
+</script>
+
+<h1>Categories</h1>
+<p>All categories: <button on:click={GetCategories}>Fetch</button></p>
+
+<table>
+	<th>
+		<tc> Id </tc>
+		<tc> Name </tc>
+	</th>
+	{#each Object.entries(categories) as [id, name]}
+		<tr>
+			<tc>
+				{id}
+			</tc>
+			<tc>
+				{name}
+			</tc>
+		</tr>
+	{/each}
+</table>
+
+<style>
+</style>
